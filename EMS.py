182e3c87ac270dba3676dcf0eee48bfeb45a6049
ATE186 <murali.r@agaramtech.com>
2022-06-10 17:12:25 +0530

    Update EMS.py

--- EMS.py
+++ EMS.py
@@ -4,10 +4,10 @@ import tkinter as tk
 
 window=tk.Tk()
 
-window.geometry("%dx%d" % (200, 200))
+window.geometry("500x500+430+50")
 
 window.title("EMS")
 
-window.config(bg="blue") 
+window.config(bg="blue")
 
 window.mainloop()
